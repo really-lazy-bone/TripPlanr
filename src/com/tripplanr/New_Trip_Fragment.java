c1a6744400694f66507aacc7fa55ae9984ab4187
Eric Liao <rcliao01@gmail.com>
2014-05-05 04:15:28 -0700

    implement basic drawer fragment switch; implement basic color structure

---
+++ src/com/tripplanr/New_Trip_Fragment.java
@@ -0,0 +1,15 @@
+package com.tripplanr;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class New_Trip_Fragment extends Fragment {
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+		View rootView = inflater.inflate(R.layout.new_trip, container, false);
+		return rootView;
+	}
+}
