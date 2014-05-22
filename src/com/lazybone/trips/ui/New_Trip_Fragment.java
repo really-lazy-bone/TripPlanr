97c7fb52043616b61b8bdad1d28421e6203d0ace
Eric Liao <rcliao01@gmail.com>
2014-05-22 11:28:42 -0700

    fixed header

--- src/com/lazybone/trips/ui/New_Trip_Fragment.java
+++ src/com/lazybone/trips/ui/New_Trip_Fragment.java
@@ -7,7 +7,6 @@ import android.app.Fragment;
 import android.app.FragmentTransaction;
 import android.content.Context;
 import android.database.Cursor;
-import android.graphics.Color;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
